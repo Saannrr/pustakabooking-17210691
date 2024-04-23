1839e2294c39d1988c414e7ed531ca4aa6ef64b4
Saannrr <ihsan.ican2002@gmail.com>
2024-04-23 11:59:07 +0700

    ganti config base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pustakabooking17210691/';
+$config['base_url'] = 'http://localhost/pustakabooking-17210691/';
 
 /*
 |--------------------------------------------------------------------------
